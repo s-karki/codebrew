3ae900e1353cdf3b64db661de0bec7fefaa78d01
Saharsha Karki <sah.karki@gmail.com>
2019-04-13 13:13:41 +1000

    working initial map

---
+++ flaskr/__init__.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import Flask, render_template, flash, redirect, url_for, request, g, \
+    jsonify
+
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        GOOGLEMAPS_KEY = "AIzaSyCVrV5t3FrHnqanqHMnPIuqn9YTZbDdqzg"
+    )
+
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    @app.route('/')
+    def mapview():
+        return render_template("index.html", key=app.config['GOOGLEMAPS_KEY'])
+
+
+
+    return app
